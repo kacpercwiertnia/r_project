fb253e96390d9c92deea562ba56b0230f67afb09
kacpercwiertnia <kacpercwiertnia02@gmail.com>
2023-01-21 20:03:51 +0100

    implemented prs algorithm

--- main.R
+++ main.R
@@ -1,3 +1,19 @@
 prs_algorithm <- function(fun, dim, num_of_points, min_range, max_range){
   f <- fun(dim);
+  min_value <- Inf
+  min_point <- Inf
+  
+  for(x in num_of_points){
+    point <- runif(dim, min_range, max_range)
+    value <- f(point)
+    
+    if(value < min_value){
+      min_value <- value
+      min_point <- point
+    }
+  }
+  
+  return(min_value)
 }
+
+prs_mean <- mean(replicate(50,prs_algorithm(makeAlpine01Function, 2, 50, -10, 10)))
