bcc3953110bac3dac98f39488d7613c34ea56e7c
kacpercwiertnia <kacpercwiertnia02@gmail.com>
2023-01-21 23:06:29 +0100

    implemented ms algorithm

--- main.R
+++ main.R
@@ -1,19 +1,35 @@
-prs_algorithm <- function(fun, dim, num_of_points, min_range, max_range){
+prs_algorithm <- function(fun, dim, num_of_points){
   f <- fun(dim);
   min_value <- Inf
-  min_point <- Inf
   
   for(x in num_of_points){
-    point <- runif(dim, min_range, max_range)
+    point <- runif(dim, getLowerBoxConstraints(f), getUpperBoxConstraints(f))
     value <- f(point)
     
     if(value < min_value){
       min_value <- value
-      min_point <- point
     }
   }
   
   return(min_value)
 }
 
-prs_mean <- mean(replicate(50,prs_algorithm(makeAlpine01Function, 2, 50, -10, 10)))
+ms_algorithm <- function(fun, dim, num_of_points){
+  f <- fun(dim);
+  min_value <- Inf
+  
+  for(x in num_of_points){
+    point <- runif(dim, getLowerBoxConstraints(f), getUpperBoxConstraints(f))
+    
+    value <- as.numeric(optim(point, f, method = "L-BFGS-B", lower = getLowerBoxConstraints(f), upper = getUpperBoxConstraints(f))["value"])
+    
+    if(value < min_value){
+      min_value <- value
+    }
+  }
+  
+  return(min_value)
+}
+
+prs_mean <- mean(replicate(50, prs_algorithm(makeAlpine01Function, 2, 1000)))
+ms_mean <- mean(replicate(50, ms_algorithm(makeAlpine01Function, 2, 1000)))
