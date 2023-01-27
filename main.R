0e52e2ee879172ca0a15c01b55fac20d0b3a79ae
kacpercwiertnia <kacpercwiertnia02@gmail.com>
2023-01-27 07:59:18 +0100

    added documentation i R markdown

--- main.R
+++ main.R
@@ -2,7 +2,7 @@ prs_algorithm <- function(fun, dim, num_of_points){
   f <- fun(dim);
   min_value <- Inf
   
-  for(x in num_of_points){
+  for(x in 1:num_of_points){
     point <- runif(dim, getLowerBoxConstraints(f), getUpperBoxConstraints(f))
     value <- f(point)
     
@@ -40,19 +40,59 @@ compare_algorithms <- function(alg1, alg2, func, dim){
   counter <- mean(alg1_counters)
   res_alg2 <- replicate(50, alg2(func, dim, counter))
   
-  hist(alg1_points, main = "Histogram algorytmu MS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
-  hist(res_alg2, main = "Histogram algorytmu PRS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
-  boxplot(alg1_points, main = "Wykres pudełkowy algorytmu MS", ylab = "Zakres minimów")
-  boxplot(res_alg2, main = "Wykres pudełkowy algorytmu PRS", ylab = "Zakres minimów")
-  t.test(x = res_alg2, y = alg1_points, conf.level = 0.95)
-  
+  return(list(alg1_points, res_alg2))
 }
 
-compare_algorithms(ms_algorithm, prs_algorithm, makeAlpine01Function, 2)
-compare_algorithms(ms_algorithm, prs_algorithm, makeAckleyFunction, 2)
+res_alpine01_2 <- compare_algorithms(ms_algorithm, prs_algorithm, makeAlpine01Function, 2)
+hist(res_alpine01_2[[1]], main = "Histogram algorytmu MS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+hist(res_alpine01_2[[2]], main = "Histogram algorytmu PRS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+boxplot(res_alpine01_2[[1]], main = "Wykres pudełkowy algorytmu MS", ylab = "Zakres minimów")
+boxplot(res_alpine01_2[[2]], main = "Wykres pudełkowy algorytmu PRS", ylab = "Zakres minimów")
+t.test(x = res_alpine01_2[[1]], conf.level = 0.95)
+t.test(x = res_alpine01_2[[2]], conf.level = 0.95)
+t.test(x = res_alpine01_2[[2]], y = res_alpine01_2[[1]], conf.level = 0.95)
 
-compare_algorithms(ms_algorithm, prs_algorithm, makeAlpine01Function, 10)
-compare_algorithms(ms_algorithm, prs_algorithm, makeAckleyFunction, 10)
+res_ackley_2 <- compare_algorithms(ms_algorithm, prs_algorithm, makeAckleyFunction, 2)
+hist(res_ackley_2[[1]], main = "Histogram algorytmu MS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+hist(res_ackley_2[[2]], main = "Histogram algorytmu PRS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+boxplot(res_ackley_2[[1]], main = "Wykres pudełkowy algorytmu MS", ylab = "Zakres minimów")
+boxplot(res_ackley_2[[2]], main = "Wykres pudełkowy algorytmu PRS", ylab = "Zakres minimów")
+t.test(x = res_ackley_2[[1]], conf.level = 0.95)
+t.test(x = res_ackley_2[[2]], conf.level = 0.95)
+t.test(x = res_ackley_2[[2]], y = res_ackley_2[[1]], conf.level = 0.95)
 
-compare_algorithms(ms_algorithm, prs_algorithm, makeAlpine01Function, 20)
-compare_algorithms(ms_algorithm, prs_algorithm, makeAckleyFunction, 20)
+res_alpine01_10 <- compare_algorithms(ms_algorithm, prs_algorithm, makeAlpine01Function, 10)
+hist(res_alpine01_10[[1]], main = "Histogram algorytmu MS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+hist(res_alpine01_10[[2]], main = "Histogram algorytmu PRS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+boxplot(res_alpine01_10[[1]], main = "Wykres pudełkowy algorytmu MS", ylab = "Zakres minimów")
+boxplot(res_alpine01_10[[2]], main = "Wykres pudełkowy algorytmu PRS", ylab = "Zakres minimów")
+t.test(x = res_alpine01_10[[1]], conf.level = 0.95)
+t.test(x = res_alpine01_10[[2]], conf.level = 0.95)
+t.test(x = res_alpine01_10[[2]], y = res_alpine01_10[[1]], conf.level = 0.95)
+
+res_ackley_10 <- compare_algorithms(ms_algorithm, prs_algorithm, makeAckleyFunction, 10)
+hist(res_ackley_10[[1]], main = "Histogram algorytmu MS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+hist(res_ackley_10[[2]], main = "Histogram algorytmu PRS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+boxplot(res_ackley_10[[1]], main = "Wykres pudełkowy algorytmu MS", ylab = "Zakres minimów")
+boxplot(res_ackley_10[[2]], main = "Wykres pudełkowy algorytmu PRS", ylab = "Zakres minimów")
+t.test(x = res_ackley_10[[1]], conf.level = 0.95)
+t.test(x = res_ackley_10[[2]], conf.level = 0.95)
+t.test(x = res_ackley_10[[2]], y = res_ackley_10[[1]], conf.level = 0.95)
+
+res_alpine01_20 <- compare_algorithms(ms_algorithm, prs_algorithm, makeAlpine01Function, 20)
+hist(res_alpine01_20[[1]], main = "Histogram algorytmu MS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+hist(res_alpine01_20[[2]], main = "Histogram algorytmu PRS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+boxplot(res_alpine01_20[[1]], main = "Wykres pudełkowy algorytmu MS", ylab = "Zakres minimów")
+boxplot(res_alpine01_20[[2]], main = "Wykres pudełkowy algorytmu PRS", ylab = "Zakres minimów")
+t.test(x = res_alpine01_20[[1]], conf.level = 0.95)
+t.test(x = res_alpine01_20[[2]], conf.level = 0.95)
+t.test(x = res_alpine01_20[[2]], y = res_alpine01_20[[1]], conf.level = 0.95)
+
+res_ackley_20 <- compare_algorithms(ms_algorithm, prs_algorithm, makeAckleyFunction, 20)
+hist(res_ackley_20[[1]], main = "Histogram algorytmu MS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+hist(res_ackley_20[[2]], main = "Histogram algorytmu PRS", xlab = "Zakresy minimów", ylab = "Ilość minimów")
+boxplot(res_ackley_20[[1]], main = "Wykres pudełkowy algorytmu MS", ylab = "Zakres minimów")
+boxplot(res_ackley_20[[2]], main = "Wykres pudełkowy algorytmu PRS", ylab = "Zakres minimów")
+t.test(x = res_ackley_20[[1]], conf.level = 0.95)
+t.test(x = res_ackley_20[[2]], conf.level = 0.95)
+t.test(x = res_ackley_20[[2]], y = res_ackley_20[[1]], conf.level = 0.95)
